37fcb366eb4ead2ade287c67cffff5c2d4eae40d
Mark Berridge <mark.berridge@gmail.com>
2014-12-22 16:24:53 +0000

    refactored view from global scope into Canvas object

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
-var canvas = document.getElementById("a");
-var ctx = canvas.getContext("2d");
-
-var sorted =   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-var reversed = [25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-var nearly =   [1, 2, 4, 5, 9, 6, 3, 7, 8, 10, 11, 13, 12, 14, 16, 15, 17, 18, 19, 21, 22, 20, 23, 25, 24];
-var random =   [9, 15, 13, 11, 8, 20, 10, 19, 6, 24, 4, 3, 1, 12, 17, 21, 23, 2, 16, 18, 25, 14, 5, 22, 7];
-
 // ######
 // ### Insertion Sort
 // ######
-
-var InsertionSort = function(context, array) {
-  this.ctx = context;
-  this.a = array;
+var InsertionSort = function(canvas) {
+  this.canvas = canvas;
   this.i = 1;
   this.k = 1;
-  this.sorted = false;
-  this.delay = 1000;
-
-  renderArray(this.ctx, this.a);
+  this.delay = 40;
 }
 
 InsertionSort.prototype.step = function(auto) {
-  
+
   console.log("step called, i=" + this.i + ", k=" + this.k);
-  console.log("a=" + this.a);
+  console.log("a=" + this.canvas.a);
 
   // Iterative algorithm:
   // for (var i = 1; i < a.length; i++) {
@@ -33,18 +20,18 @@ InsertionSort.prototype.step = function(auto) {
   //    }
   // }
 
-  if(this.a[this.k] < this.a[this.k-1]) {
-    swap(this.a, this.k, this.k - 1);
-    renderArray(this.ctx, this.a);
+  if(this.canvas.a[this.k] < this.canvas.a[this.k - 1]) {
+    this.canvas.swap(this.k, this.k - 1);
   }
   
-  while (this.k > 0 && this.a[this.k] > this.a[this.k-1]) {
-	  this.k--;
+  while (this.k > 0 && this.canvas.a[this.k] > this.canvas.a[this.k - 1]) {
+    this.k--;
   }
-  
+
   if(this.k == 0) {
-    if(this.i < this.a.length) {
-      this.i++;
+    // TODO highlight element i
+    this.i++
+    if(this.i < this.canvas.a.length) {
       this.k = this.i;
     }
     else {
@@ -52,11 +39,11 @@ InsertionSort.prototype.step = function(auto) {
       return;
     }
   }
-  
+
   if(auto) {
     setTimeout((function() {
-      this.start(true);
-    }).bind(this), 40);
+      this.step(true);
+    }).bind(this), this.delay);
   }
 }
 
@@ -66,35 +53,51 @@ InsertionSort.prototype.start = function() {
 
 
 //######
-//### Canvas - needs encapsulating
+//### Canvas
 //######
-
-function swap(a, x, y) {
-  console.log("Swapping " + a[y] + " and " + a[x]);
-  var z = a[y];
-  a[y] = a[x];
-  a[x] = z;
+var Canvas = function(id, array) {
+  // TODO check element with id exists and is a canvas...
+  this.canvas = document.getElementById(id);
+  this.ctx = this.canvas.getContext("2d");
+  this.a = array;
+  this.renderArray();
 }
 
-function renderArray(ctx, a) {
-  for (var i = 0; i < a.length; i++) {
-    renderBar(ctx, i, a[i]);
+Canvas.prototype.swap = function (x, y) {
+  console.log("Swapping " + this.a[y] + " and " + this.a[x]);
+  var z = this.a[y];
+  this.a[y] = this.a[x];
+  this.a[x] = z;
+  this.renderArray();
+}
+
+Canvas.prototype.renderArray = function() {
+  for (var i = 0; i < this.a.length; i++) {
+    this.renderBar(i, this.a[i]);
   }
 }
 
-function renderBar(ctx, index, length) {
+Canvas.prototype.renderBar = function(index, length) {
   var scale = 7;
   var x = scale * 2;
   var y = (index + 1) * (scale + 1);
   var width = length * scale;
   var height = scale;
-  // fill bar
-  ctx.fillRect(x, y, width, height);
+   // fill bar
+  this.ctx.fillRect(x, y, width, height);
   // clear after bar
-  ctx.clearRect (x + width, y, canvas.width - (x + width), height);
+  this.ctx.clearRect (x + width, y, this.canvas.width - (x + width), height);
 }
 
 //######
+var reversed = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+var nearly =   [1, 2, 4, 5, 9, 6, 3, 7, 8, 10, 11, 13, 12, 14, 16, 15, 17, 20, 18, 19];
+var random =   [9, 15, 13, 11, 8, 20, 10, 19, 6, 4, 3, 1, 12, 17, 2, 16, 18, 14, 5, 7];
 
-var sort = new InsertionSort(ctx, random);
-sort.start();
+var a = new InsertionSort(new Canvas("a", nearly));
+var b = new InsertionSort(new Canvas("b", random));
+var c = new InsertionSort(new Canvas("c", reversed));
+
+a.start();
+b.start();
+c.start();
